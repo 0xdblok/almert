949f2b6007adf1b8ee69d012426f10d1aebeb7f6
dblok0x <dblok0x@gmail.com>
2024-11-26 15:30:14 +0100

    ee

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import logo from "./logo.jpg";
-import bann from "./bann.jpg";
+import banner from "./bannner.png";
 import Hero from "./hero";
 import Tokenomics from "./tokenomics";
 import Footer from "./footer";
@@ -13,7 +13,16 @@ export default function Home() {
     <Hero />
     <div className="flex justify-center">
     <a className="relative z-0 h-14 flex justify-center items-center rounded-full bg-[#00C8FF] px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-[#34a5c5] hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500">ʙᴜʏ ᴏɴ ᴜɴɪꜱᴡᴀᴘ</a>
-  </div>  <Tokenomics />
+  </div> 
+  <div className="mt-20 mx-4 border-4 w-full h-full">
+  <Image
+        className="w-full h-full  "
+        src={banner}
+        alt="Nikula Tisla"
+        width={350}
+        height={350}
+      /></div>
+   <Tokenomics />
 
     <Footer />
   </div>
