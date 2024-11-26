442d2a99280dde94f7099911adcc0f99e5ad2a4d
dblok0x <dblok0x@gmail.com>
2024-11-26 16:25:14 +0100

    ee

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Almert Eynstayn",
   description: "Almert Eynstayn",
+ 
 };
 
 export default function RootLayout({ children }) {
