a48d21f8b386ac0762a9524175c0d4833f3c3cb5
dblok0x <dblok0x@gmail.com>
2024-11-26 15:52:42 +0100

    ee

--- app/hero.js
+++ app/hero.js
@@ -9,8 +9,8 @@ export default function Hero() {
         className="mx-auto sm:w-10/12 lg:w-2/3 "
         src={logo}
         alt="Nikula Tisla"
-        width={450}
-        height={450}
+        width={400}
+        height={400}
       />{" "}
     </div>{" "}
     <div className="space-y-4 flex-1 sm:text-center lg:text-left ">
@@ -18,7 +18,7 @@ export default function Hero() {
       𝘼𝙇𝙈<span className="text-[#2055A4]">𝙀𝙍𝙏</span>{" "}
         <span className="text-[#2055A4]">  𝙀𝙔𝙉 </span>𝙎𝙏𝘼𝙔𝙉
       </h1>
-      <p className="bunge  md:px-48 text-center text-lg font-medium leading-relaxed sm:mx-auto lg:ml-0">
+      <p className="bunge  md:px-28 text-center text-lg font-medium leading-relaxed sm:mx-auto lg:ml-0">
       ɪ, ᴀʟᴍᴇʀᴛ ᴇʏɴꜱᴛᴀʏɴ (ɴᴏᴛ ᴛᴏ ʙᴇ ᴄᴏɴꜰᴜꜱᴇᴅ ᴡɪᴛʜ ᴛʜᴀᴛ ᴏᴛʜᴇʀ ɢᴜʏ), ᴅɪꜱᴄᴏᴠᴇʀᴇᴅ ꜱᴏᴍᴇᴛʜɪɴɢ ʀᴇᴠᴏʟᴜᴛɪᴏɴᴀʀʏ ᴡʜɪʟᴇ ᴇᴀᴛɪɴɢ ᴄʀᴀʏᴏɴꜱ ɪɴ ᴍʏ ʙᴀꜱᴇ ʙʟᴏᴄᴋᴄʜᴀɪɴ ᴛʀᴇᴇʜᴏᴜꜱᴇ. ᴡɪᴛʜ ᴍʏ ᴄᴇʀᴛɪꜰɪᴇᴅ 37 ɪQ (ᴛᴇꜱᴛᴇᴅ ʙʏ ᴍʏ ᴘᴇᴛ ʜᴀᴍꜱᴛᴇʀ), ɪ ʜᴀᴠᴇ ʙᴇᴇɴ ᴄʜᴏꜱᴇɴ ᴛᴏ ᴄʀᴇᴀᴛᴇ ꜱᴏᴍᴇᴛʜɪɴɢ ᴛʜᴀᴛ ᴡɪʟʟ ᴄʜᴀɴɢᴇ ᴇᴠᴇʀʏᴛʜɪɴɢ, ᴍᴀʏʙᴇ.
       </p>
     </div>
