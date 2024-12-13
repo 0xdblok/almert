566e20e11bc8c5c893e6fd5ea695b44a2a8a3f60
dblok0x <dblok0x@gmail.com>
2024-12-13 11:45:17 +0100

    rr

--- app/footer.js
+++ app/footer.js
@@ -7,10 +7,12 @@ export default function Footer() {
   return (
     <div className="flex pt-6 justify-end pb-6 px-8">
     <div className="flex gap-2">
+
+     
       {" "}
 
       <a
-        href=""
+        href="https://x.com/almerteynstayn/"
         className="flex items-center font-mono font-medium text-blue-600 hover:underline ">
         <Image
           className=" md:w-[35px] md:h-[35px]"
